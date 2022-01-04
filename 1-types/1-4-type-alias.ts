448fb6cfe432de481cfd47b30d8d04d50f78c801
GW <kowonp@gmail.com>
2022-01-04 17:54:10 +0900

    1-4-type-alias

---
+++ 1-types/1-4-type-alias.ts
@@ -0,0 +1,30 @@
+{
+  /**
+   * Type Alias
+   */
+  type Num = number;
+  type Text = string;
+
+  const num1: Num = 10;
+  const name: string = "gw";
+  const address: Text = "kr";
+
+  type Student = {
+    name: string;
+    age: number;
+  };
+
+  const student: Student = { name: "gw", age: 10 };
+
+  /**
+   * String Literal Types
+   */
+  type Name = "name";
+  let myName: Name;
+  myName = "name";
+  // myName = "hello"; // Type '"hello"' is not assignable to type '"name"'.
+
+  type User = "user";
+  let myUser: User;
+  myUser = "user";
+}

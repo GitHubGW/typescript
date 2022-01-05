3dd34c8c57b09d0923b450ada84f5e082156b8a6
GW <kowonp@gmail.com>
2022-01-05 18:17:50 +0900

    1-6-discriminated-union

---
+++ 1-types/1-6-discriminated-union.ts
@@ -0,0 +1,68 @@
+{
+  /**
+   * Discriminated Union Types
+   */
+
+  /**
+   * function: login -> success, fail
+   */
+  type LoginState = SuccessState | FailState;
+  type SuccessState = {
+    result: "success";
+    body: string;
+  };
+  type FailState = {
+    result: "fail";
+    reason: string;
+  };
+
+  function login(id: string, password: string): LoginState {
+    if (id && password) {
+      return { result: "success", body: "success" };
+    }
+
+    return { result: "fail", reason: "fail" };
+  }
+
+  // printLoginState(state: LoginState) -> success, fail
+  function printLoginState(state: LoginState): void {
+    // state.result -> success or fail
+    if (state.result === "success") {
+      console.log(state.body);
+    } else {
+      console.log(state.reason);
+    }
+  }
+
+  /**
+   *  Example
+   */
+  type User = Owner | Delivery;
+  type Owner = {
+    name: "owner";
+    owner: string;
+  };
+  type Delivery = {
+    name: "delivery";
+    delivery: string;
+  };
+
+  /* 
+  이 방법보다는 아래 방법을 좀 더 추천
+  function handleUser(user: Owner | Delivery):void {
+    if ("owner" in user) {
+      console.log(user.owner);
+    } else if ("delivery" in user) {
+      console.log(user.delivery);
+    }
+  } 
+  */
+
+  function handleUser(user: User): void {
+    if (user.name === "owner") {
+      console.log(user.owner);
+    } else if (user.name === "delivery") {
+      console.log(user.delivery);
+    }
+  }
+}
